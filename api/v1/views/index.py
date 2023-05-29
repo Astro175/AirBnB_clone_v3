68933fd556f86837c792fa7b4f4d44f1b15a0821
astro175 <aspark400@gmail.com>
2023-05-29 01:16:04 +0100

    added task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,13 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -15,8 +22,9 @@ def status():
                  strict_slashes=False)
 def count():
     """Retrieves the number of objects"""
-    obj_type = [Amenities, State, City, Review, Place, User]
+    obj_type = [Amenity, State, City, Review, Place, User]
     stats = {}
     for obj in obj_type:
-        stats_result = count(obj)
+        stats_result = storage.count(obj)
         stats[obj] = stats_result
+    return jsonify(stats)
